51e75396fa6d3f303efa4289bc67409419739d81
bilala61 <bilala61@kingshigh.school.nz>
2026-06-12 12:24:42 +1200

    iewqfp'qe

--- main_menu.gd
+++ main_menu.gd
@@ -1,15 +1,12 @@
 extends Control
 
-func _ready():
-	GameState.reset_game()
-	$PlayButton.pressed.connect(_on_play_pressed)
-	# Animate title
-	var tween = create_tween()
-	tween.set_loops()
-	tween.tween_property($Title, "modulate", Color(1, 0.8, 0.2), 1.0)
-	tween.tween_property($Title, "modulate", Color(1, 0.4, 0.6), 1.0)
-	tween.tween_property($Title, "modulate", Color(0.4, 0.8, 1.0), 1.0)
-	tween.tween_property($Title, "modulate", Color(1, 0.8, 0.2), 1.0)
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/deliverymap.tscn")
+func _on_button_pressed() -> void:
+	print ("START button pressed")
+	get_tree().change_scene_to_file("res://delivery_map.tscn")
+
+
+
+func _on_button_2_pressed() -> void:
+	print ("EXIT button pressed")
+	get_tree().quit()

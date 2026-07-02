0fdad9e4863b6ac1c2f61593aaadf2224f00b2cc
bilala61 <bilala61@kingshigh.school.nz>
2026-07-02 14:57:52 +1200

    i did some more scrips

--- script 2 game/player.gd
+++ script 2 game/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const speed = 100
+var current_dir = "none"
 
 func _physics_process(delta):
 	player_movement(delta)
@@ -8,22 +9,62 @@ func _physics_process(delta):
 func player_movement(delta):
 	pass
 	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
 		velocity.x = speed 
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed 
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
 		velocity.y = speed 
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
 		velocity.y = -speed 
 		velocity.x = 0
 	else:
+		play_anim(0)
 		velocity.x = 0 
 		velocity.y = 0 
 	
 	move_and_slide()
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	 
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("Side_walk")
+		elif movement == 0:
+			anim.play("side_Idle")
 	
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("Side_walk")
+		elif movement == 0:
+			anim.play("Side_Idle")
+	
+	if dir == "down":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("Front_walk")
+		elif movement == 0:
+			anim.play("Front_Idle")
+			
+	if dir == "up":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("Back_walk")
+		elif movement == 0:
+			anim.play("Back_Idle")
 	
 	

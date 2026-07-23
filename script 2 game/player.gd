b8c2e3f5e90a1c3e51c0bf815e5a8fc59a12ca3d
bilala61 <bilala61@kingshigh.school.nz>
2026-07-23 13:25:53 +1200

    v1.2

--- script 2 game/player.gd
+++ script 2 game/player.gd
@@ -13,22 +13,22 @@ func _physics_process(delta):
 
 func player_movement(delta):
 	pass
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("right"):
 		current_dir = "right"
 		play_anim(1)
 		velocity.x = speed 
 		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("left"):
 		current_dir = "left"
 		play_anim(1)
 		velocity.x = -speed 
 		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("down"):
 		current_dir = "down"
 		play_anim(1)
 		velocity.y = speed 
 		velocity.x = 0
-	elif Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("up"):
 		current_dir = "up"
 		play_anim(1)
 		velocity.y = -speed 
